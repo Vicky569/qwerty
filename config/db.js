15642d21369ae87d5e3b0f731452862f6e371f92
Vikram Kumar Saw <102428453+Vicky569@users.noreply.github.com>
2023-04-08 00:13:33 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
+    const conn = await mongoose.connect("mongodb+srv://vicky:wQ3k9JTsHFUgpIBr@cluster0.vi2uszv.mongodb.net/?retryWrites=true&w=majority", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
